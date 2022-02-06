917823ff262ba67361ee23cbc54d1bd22980331c
Eugene <72232749+EugeneIm@users.noreply.github.com>
2022-02-05 22:49:59 -0800

    Add files via upload
    created a repo for api-cs

---
+++ daytwo/day-two/day-two/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace day_two.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProduceID { get; set; }
+        public string Description { get; set; }
+    }
+
+}
